b87e1274dfcc422e82985d1edf25906119d3c00a
joshu <joshu@BITANCUTIE>
2025-04-10 11:50:25 +0800

    updated bago kumain

--- BasuraHero/User.h
+++ BasuraHero/User.h
@@ -56,6 +56,9 @@ namespace BasuraHero {
 
 
 	private: System::Windows::Forms::Label^ btnMenu;
+	private: System::Windows::Forms::PictureBox^ picF;
+	private: System::Windows::Forms::PictureBox^ picM;
+
 
 	protected:
 
@@ -76,6 +79,10 @@ namespace BasuraHero {
 			this->txtUser = (gcnew System::Windows::Forms::TextBox());
 			this->comboGender = (gcnew System::Windows::Forms::ComboBox());
 			this->btnMenu = (gcnew System::Windows::Forms::Label());
+			this->picF = (gcnew System::Windows::Forms::PictureBox());
+			this->picM = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picF))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picM))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// txtUser
@@ -93,13 +100,15 @@ namespace BasuraHero {
 			// 
 			// comboGender
 			// 
-			this->comboGender->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->comboGender->DropDownStyle = System::Windows::Forms::ComboBoxStyle::DropDownList;
+			this->comboGender->Font = (gcnew System::Drawing::Font(L"Britannic Bold", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->comboGender->FormattingEnabled = true;
 			this->comboGender->Items->AddRange(gcnew cli::array< System::Object^  >(2) { L"Male", L"Female" });
-			this->comboGender->Location = System::Drawing::Point(243, 554);
+			this->comboGender->Location = System::Drawing::Point(243, 558);
 			this->comboGender->Name = L"comboGender";
-			this->comboGender->Size = System::Drawing::Size(121, 33);
+			this->comboGender->RightToLeft = System::Windows::Forms::RightToLeft::No;
+			this->comboGender->Size = System::Drawing::Size(121, 38);
 			this->comboGender->TabIndex = 1;
 			this->comboGender->SelectedIndexChanged += gcnew System::EventHandler(this, &User::comboBox1_SelectedIndexChanged);
 			// 
@@ -108,15 +117,37 @@ namespace BasuraHero {
 			this->btnMenu->AutoSize = true;
 			this->btnMenu->BackColor = System::Drawing::Color::Transparent;
 			this->btnMenu->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btnMenu->Font = (gcnew System::Drawing::Font(L"Impact", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->btnMenu->Font = (gcnew System::Drawing::Font(L"Britannic Bold", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
 			this->btnMenu->ForeColor = System::Drawing::Color::White;
-			this->btnMenu->Location = System::Drawing::Point(276, 713);
+			this->btnMenu->Location = System::Drawing::Point(270, 713);
 			this->btnMenu->Name = L"btnMenu";
-			this->btnMenu->Size = System::Drawing::Size(88, 39);
+			this->btnMenu->Size = System::Drawing::Size(94, 36);
 			this->btnMenu->TabIndex = 3;
 			this->btnMenu->Text = L"MENU";
 			this->btnMenu->Click += gcnew System::EventHandler(this, &User::btnMenu_Click);
+			// 
+			// picF
+			// 
+			this->picF->BackColor = System::Drawing::Color::Transparent;
+			this->picF->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->picF->Location = System::Drawing::Point(220, 476);
+			this->picF->Name = L"picF";
+			this->picF->Size = System::Drawing::Size(62, 62);
+			this->picF->TabIndex = 4;
+			this->picF->TabStop = false;
+			this->picF->Click += gcnew System::EventHandler(this, &User::picF_Click);
+			// 
+			// picM
+			// 
+			this->picM->BackColor = System::Drawing::Color::Transparent;
+			this->picM->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->picM->Location = System::Drawing::Point(326, 476);
+			this->picM->Name = L"picM";
+			this->picM->Size = System::Drawing::Size(62, 62);
+			this->picM->TabIndex = 5;
+			this->picM->TabStop = false;
+			this->picM->Click += gcnew System::EventHandler(this, &User::picM_Click);
 			// 
 			// User
 			// 
@@ -125,6 +156,8 @@ namespace BasuraHero {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(600, 800);
+			this->Controls->Add(this->picM);
+			this->Controls->Add(this->picF);
 			this->Controls->Add(this->btnMenu);
 			this->Controls->Add(this->comboGender);
 			this->Controls->Add(this->txtUser);
@@ -134,6 +167,8 @@ namespace BasuraHero {
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"User";
 			this->Load += gcnew System::EventHandler(this, &User::User_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picF))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->picM))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -181,6 +216,12 @@ private: System::Void btnMenu_Click(System::Object^ sender, System::EventArgs^ e
 	this->switchToStart = true;
 	this->DialogResult = System::Windows::Forms::DialogResult::OK;
 	this->Close();
+}
+private: System::Void picF_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->comboGender->SelectedIndex = 1; // Selects the first item (Female)
+}
+private: System::Void picM_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->comboGender->SelectedIndex = 0; // male
 }
 };
 }
